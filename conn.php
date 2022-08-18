4c006b489ce8143bc678205b6fd38775e895b683
Ritik Rana <ranaritik2549@gmail.com>
2022-08-18 21:24:14 +0530

    Conn File Added and working on registerF file

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sih";
+    
+    // Connecting to database
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if(! $conn)
+{
+   die('Connection Failed'.mysql_error());
+}
+?>
