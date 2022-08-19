4969ae8a33c0c9f3c6c474fdb129fc0c83fdc92b
Ritik Rana <ranaritik2549@gmail.com>
2022-08-19 17:25:26 +0530

    ritik rana

--- regesterB.php
+++ regesterB.php
@@ -53,7 +53,7 @@
   
     <section class="text-gray-600 body-font relative">
       <div class="container px-5 py-24 mx-auto">
-      <form action="regesterB.html" method="post">
+      <form action="regesterB.php" method="post">
         <div class="flex flex-col text-center w-full mb-8">
           <h1
             class="sm:text-3xl text-2xl font-medium title-font mb-2 text-blue-200"
