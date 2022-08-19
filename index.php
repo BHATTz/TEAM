57744eaa07e5ef2c165c3dc260c29432ae0177c0
SARTHAK BHATT <sarthakbhatt0@gmail.com>
2022-08-19 10:51:15 +0530

    bhatt

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="main.css" />
+  </head>
+
+  <body style="background-color:#111827">
+    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+    
+    <!-- header -->
+
+    <header class="text-gray-400 bg-gray-900 body-font">
+      <div
+        class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+      >
+        <a
+          class="flex title-font font-serif items-center text-white mb-4 md:mb-0"
+        >
+          <a href="index.html" class="active">
+            <img src="img/photography.png" alt="logo" height="50" width="50"
+          /></a>
+          <span class="ml-3 text-xl"
+            ><a href="index.html" class="active">YourDailydDairyCart</a></span
+          >
+        </a>
+        <nav
+          class="md:ml-auto flex flex-wrap items-center text-base justify-center font-serif"
+        >
+          <a class="mr-5 hover:text-white" a href="aboutus.php">About Us</a>
+          <a class="mr-5 hover:text-white" a href="tips.html">Tips</a>
+          <a class="mr-5 hover:text-white" a href="team.html">Team</a>
+        </nav>
+
+        <button
+          class="font-serif inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          <a href="login.html">Login</a>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+
+    <!-- content -->
+
+    <header class="header">
+      <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-40 py-52 mx-auto">
+          <div class="flex flex-wrap -m-12">
+            <div class="p-12 md:w-1/2 flex flex-col items-start">
+              <span class="inline-block py-1 px-2 rounded bg-indigo-200 text-black text-2xl font-medium tracking-widest">"I WANT MY MILK"</span>
+              <br>
+              <p class="leading-relaxed mb-8 text-white text-lg font-serif">“Do what you love to do, and be around things that make you smile. The cows make me smile every day.” </p>
+              <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
+              
+                <a class="text-indigo-500 inline-flex items-center text-2xl "href="#p1">Lets Go
+                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+                </span>
+              </div>
+              <a class="inline-flex items-center">
+                <img alt="blog" src="img/photography.png" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                <span class="flex-grow flex flex-col pl-4">
+                  <span class="title-font font-medium text-gray-100 text-xl ">YourDailydDairyCart</span>
+                  <span class="text-gray-200 text-base tracking-widest mt-0.5">© 2022 SGGS Team 3</span>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </header>
+
+<header class="header1"id="p1"></header>
+<br>
+
+<!-- sub content 1-->
+
+    <br><div
+    class="max-w-3xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800"
+  >
+    <div class="mt-2">
+      <a
+      class="text-2xl font-bold text-gray-700 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200"
+      >A portal for both dairy farmers and dairy product organizations. Increase the consumption of dairy products.  India’s 75 million dairy farms now produce more milk than all of the European Union. "Purity and Fresh Product is the Identity of our Website".</a
+    >
+    </div>
+    </div>
+
+    <section class="text-gray-600 body-font ">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="inline-block w-8 h-8 text-gray-400 mb-8"
+            viewBox="0 0 975.036 975.036"
+          >
+            <path
+              d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"
+            ></path>
+          </svg>
+          <p class="leading-relaxed text-lg">
+            In the Seventies, women runners, developing amenorrhea and
+            calcium-related shin splints, were the first to realize that
+            nature is hovering over us, ready to shut down our systems if our
+            fetus-feeding fat reserve drops below a certain percentage of body
+            weight. In other words, in nature's eyes we are nothing but milk
+            sacs and fat deposits.
+          </p>
+          <span
+            class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"
+          ></span>
+          <h2
+            class="text-gray-900 font-medium title-font tracking-wider text-sm"
+          >
+            Camille Paglia
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <div
+    class="max-w-3xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800"
+  >
+    <div class="mt-2">
+      <a
+      class="text-2xl font-bold text-gray-700 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200"
+      >On the broad spectrum of solitude, I lean toward the extreme end:
+      I work alone, as well as live alone, so I can pass an entire day
+      without uttering so much as a hello to another human being.
+      Sometimes a day's conversation consists of only five words,
+      uttered at the local Starbucks: 'Large coffee with milk, please.</a
+    >
+    </div>
+    </div>
+
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+          <div class="sm:w-1/2 mb-10 px-4">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img
+                alt="content"
+                class="object-cover object-center h-full w-full"
+                src="https://source.unsplash.com/1201x501/?milk"
+              />
+            </div>
+          </div>
+
+          <div class="sm:w-1/2 mb-10 px-4">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img
+                alt="content"
+                class="object-cover object-center h-full w-full"
+                src="https://source.unsplash.com/1201x501/?dairy"
+              />
+            </div>
+          </div>
+          <div class="sm:w-1/2 mb-10 px-4">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img
+                alt="content"
+                class="object-cover object-center h-full w-full"
+                src="https://source.unsplash.com/1201x501/?cow"
+              />
+            </div>
+          </div>
+
+          <div class="sm:w-1/2 mb-10 px-4">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img
+                alt="content"
+                class="object-cover object-center h-full w-full"
+                src="https://source.unsplash.com/1201x501/?water buffalo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="inline-block w-8 h-8 text-gray-400 mb-8"
+            viewBox="0 0 975.036 975.036"
+          >
+            <path
+              d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"
+            ></path>
+          </svg>
+          <p class="leading-relaxed text-lg">
+            I am a “cookies and milk” man. Not too old for milk. Oh no, I
+            bought wine and not milk again. My go-to drink. What’ll I do
+            without milk? Milk… One glass is not enough. I want my milk. Milk
+            – As good as it gets! Get healthy. Get milk. An utterly appealing
+            beverage. Milk moments. Almond milk is what’s in my glass. Milky
+            refreshing. My favorite drink in the fridge. Milk for everyone.
+            Milk gets you goin’. Soy milk. Drink something better. I want
+            more. I want milk. Enjoy an island of coconut milk. Milk and me…
+            Forever. Think. Feel. Milk. Get ready for goat milk. A cattle call
+            for healthy beverages. Keep calm and drink some milk Think fresh.
+            Milk brings out the best in us. Milk strong! Munching a milk-based
+            diet. All milk, All the time. Freshness all day. Milky goodness.
+            Healthy. Nutritious. Milk. Always fresh from the utter.
+          </p>
+          <span
+            class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"
+          ></span>
+        </div>
+      </div>
+    </section>
+  </header>
+
+    <!-- footer -->
+
+    <footer class="text-gray-400 bg-gray-900 body-font">
+      <div class="container px-24 py-5 mx-auto">
+        <div class="flex flex-wrap md:text-left text-center order-first">
+          <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2
+              class="title-font font-medium text-white tracking-widest text-sm mb-3"
+            >
+              CATEGORIES
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="mr-5 hover:text-white" a href="aboutus.html#second">ABOUT US</a>
+              </li>
+              <li>
+              <a class="mr-5 hover:text-white" a href="tips.html">TIPS</a>
+            </li>
+            <li>
+          <a class="mr-5 hover:text-white" a href="team.html">TEAM</a>
+        </li>
+        <li>
+        <button class="inline-flex items-center focus:outline-none mr-5 hover:text-white"
+         a href="http://127.0.0.1:5500/login.html">Login</a>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+        </li>
+        </nav>
+            </nav>
+          </div>
+          <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2
+              class="title-font font-medium text-white tracking-widest text-sm mb-3"
+            >
+              TEAM MEMBERS
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-gray-400 hover:text-white">SARTHAK BHATT</a>
+              </li>
+              <li>
+                <a class="text-gray-400 hover:text-white">RITIK RANA</a>
+              </li>
+              <li>
+                <a class="text-gray-400 hover:text-white">GAURAV KUMAR</a>
+              </li>
+              <li>
+                <a class="text-gray-400 hover:text-white">SONU VERMA</a>
+              </li>
+            </nav>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2
+              class="title-font font-medium text-white tracking-widest text-sm mb-3"
+            >
+            FEEDBACK
+            </h2>
+            <div
+              class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start"
+            >
+              <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                <label
+                  for="footer-field"
+                  class="leading-3 text-sm text-gray-400"
+                  >We all need people who will give us feedback. That’s how we improve.</label
+                >
+              </div>
+              <button
+                class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded"
+              ><a href="http://127.0.0.1:5500/aboutus.html">Press Me</a>
+             </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-800 bg-opacity-75">
+        <div
+          class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col"
+        >
+          <a
+            class="flex title-font font-medium items-center md:justify-start justify-center text-white"
+          >
+            <a href="index.html" class="active">
+              <img src="img/photography.png" alt="logo" height="50" width="50"
+            /></a>
+            <span class="ml-3 text-xl"
+              ><a href="index.html" class="active">YourDailydDairyCart</a></span
+            >
+          </a>
+          <p class="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">
+            © 2022 SGGS Team 3 —
+            <a
+              href="https://twitter.com/knyttneve"
+              class="text-gray-500 ml-1"
+              target="_blank"
+              rel="noopener noreferrer"
+              >@YourDailydDairyCart</a
+            >
+          </p>
+          <span
+            class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
+          >
+          <a
+          href="#"
+          class="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300 group-hover:text-white"
+          aria-label="Github"
+        >
+          <svg
+            class="w-6 h-6 fill-current"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"
+            ></path>
+          </svg>
+        </a>
+          </span>
+        </div>
+      </div>
+    </footer>
+    <script src="js.js"></script>
+  </body>
+</html>
