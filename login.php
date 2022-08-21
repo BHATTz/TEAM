642b76c961017004f4d3739f012226e32e9be00c
SARTHAK BHATT <sarthakbhatt0@gmail.com>
2022-08-21 12:44:41 +0530

    c

--- login.php
+++ login.php
@@ -76,6 +76,7 @@
               class="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
+          <input type="submit" name="submit" vlaue="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"></input>
           <button
           class="text-xs text-gray-500 mt-3">forgot Password -></button>
       </div>
@@ -103,7 +104,7 @@
            ?>
            <script>
                alert("login successfully");
-              location.replace("business.php");
+              location.replace("farmer.php");
            </script>
            <?php
          }
@@ -136,7 +137,7 @@
           ?>
           <script>
               alert("login successfully");
-             location.replace("farmer.php");
+             location.replace("business.php");
           </script>
           <?php
         }
