6170885071fb2a688258668f5159c3dc30af0bb7
SARTHAK BHATT <sarthakbhatt0@gmail.com>
2022-08-25 23:35:40 +0530

    bhatt

--- farmer.php
+++ farmer.php
@@ -33,7 +33,7 @@
           <button
           class="font-serif inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
-          <a href="#">Profile</a>
+          <a href="dbfarmer.php">Profile</a>
         </nav>
         &nbsp;&nbsp;
         <button
