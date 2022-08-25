70e79055aa8fa55579a773d214258ab50e43ec70
SARTHAK BHATT <sarthakbhatt0@gmail.com>
2022-08-25 15:19:04 +0530

    BHATT

--- business.php
+++ business.php
@@ -8,7 +8,47 @@
   <body style="background-color: #111827">
     <!-- header -->
 
-    <?php include("include/header.php"); ?>
+       
+<header class="text-gray-400 bg-gray-900 body-font">
+      <div
+        class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+      >
+        <a
+          class="flex title-font font-serif items-center text-white mb-4 md:mb-0"
+        >
+          <a href="index.php" class="active">
+            <img src="img/photography.png" alt="logo" height="50" width="50"
+          /></a>
+          <span class="ml-3 text-xl"
+            ><a href="index.php" class="active">YourDailydDairyCart</a></span
+          >
+        </a>
+        <nav
+          class="md:ml-auto flex flex-wrap items-center text-base justify-center font-serif"
+        >
+          <a class="mr-5 hover:text-white"href="aboutus.php">About Us</a>
+          <a class="mr-5 hover:text-white"href="tips.php">Tips</a>
+          <a class="mr-5 hover:text-white"href="team.php">Team</a>
+        </nav>
+
+        <button
+          class="font-serif inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          <a href="login.php">LogOut</a>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
 
     <!-- content -->
     <?php
