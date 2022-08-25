1693c76c98ebc3abdbd43d8ecde5d49f1ca5ddd0
SARTHAK BHATT <sarthakbhatt0@gmail.com>
2022-08-25 22:30:21 +0530

    v

--- farmer.php
+++ farmer.php
@@ -29,6 +29,12 @@
           <a class="mr-5 hover:text-white"href="aboutus.php">About Us</a>
           <a class="mr-5 hover:text-white"href="tips.php">Tips</a>
           <a class="mr-5 hover:text-white"href="team.php">Team</a>
+
+          <button
+          class="font-serif inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          <a href="#">Profile</a>
+
         </nav>
 
         <button
